445c5d5828950c922460fc2ef1321f4796c39e74
Marcelo Soares <marcelossoares@live.com>
2022-05-03 03:06:20 -0300

    Insert implementation

--- src/model/dao/impl/SellerDaoJBDC.java
+++ src/model/dao/impl/SellerDaoJBDC.java
@@ -1,6 +1,7 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -25,8 +26,39 @@ public class SellerDaoJBDC implements SellerDao {
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-
+		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller"
+					+"(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+					+"VALUES"
+					+"(?, ?, ?, ?, ?)");
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0){
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+					
+					DB.closeResultSet(rs);
+				}
+			}else {
+				throw new DbException("No rows affected");
+			}
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
